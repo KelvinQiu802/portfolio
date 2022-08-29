696a09854c61ff35b4d5facac2ac1cbd216cb7ce
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-29 21:55:49 +0800

    feat: add index page title

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Head>
+        <title>Kelvin Qiu</title>
+      </Head>
+      Hello
+    </div>
+  );
 };
 
 export default Home;
