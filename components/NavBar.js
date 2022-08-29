32510ed41a92c8749ef13656cbd79b4ef0cea9ee
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-29 23:12:09 +0800

    feaet: nav bar mobile

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,15 +6,35 @@ const NavBar = () => {
     <div className={styles.container}>
       <span className={styles.signature}>Kelvin</span>
       <nav className={styles.nav}>
+        {/* Mobile Icon */}
         <Link href='/'>
-          <a className={styles.link}>Blog</a>
+          <span className={`iconfont ${styles.mobile} ${styles.icon}`}>
+            &#xe634;
+          </span>
         </Link>
         <Link href='/'>
-          <a className={styles.link}>Projects</a>
+          <span className={`iconfont ${styles.mobile} ${styles.icon}`}>
+            &#xe650;
+          </span>
         </Link>
         <Link href='/'>
-          <a className={styles.link}>Videos</a>
+          <span className={`iconfont ${styles.mobile} ${styles.icon}`}>
+            &#xe54d;
+          </span>
         </Link>
+
+        {/* Laptop Link */}
+        <Link href='/'>
+          <a className={`${styles.link} ${styles.laptop}`}>Blog</a>
+        </Link>
+        <Link href='/'>
+          <a className={`${styles.link} ${styles.laptop}`}>Projects</a>
+        </Link>
+        <Link href='/'>
+          <a className={`${styles.link} ${styles.laptop}`}>Videos</a>
+        </Link>
+
+        {/* Other Icon */}
         <Link href='/'>
           <span className={`iconfont ${styles.icon}`}>&#xe885;</span>
         </Link>
