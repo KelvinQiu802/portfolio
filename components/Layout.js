502db70ea307dae4b7eb5f602e8e2f6c7a7a630f
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-29 23:17:07 +0800

    fix: crossOrigin

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
     <div className='layout' color-mode='light'>
       <Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
         <link
           href='https://fonts.googleapis.com/css2?family=Cedarville+Cursive&family=Inter:wght@300;400;500;600&display=swap'
           rel='stylesheet'
