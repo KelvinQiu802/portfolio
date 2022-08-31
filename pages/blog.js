d583e1ed439af2b28c3d8f79f924c515387f1480
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-31 21:05:55 +0800

    try

--- pages/blog.js
+++ pages/blog.js
@@ -1,4 +1,4 @@
-import Content from '../components/blog/content';
+import Content from '../components/Blog/content';
 import Head from 'next/head';
 import { getSortedPostData } from '../utils/postTools';
 
