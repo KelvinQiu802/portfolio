5761bbdec3447ffad002677a5c828d6d76f09cbe
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-31 19:42:55 +0800

    feaet: adjust margin

--- components/Blog/Content.js
+++ components/Blog/Content.js
@@ -5,6 +5,10 @@ const Content = () => {
   return (
     <Wrapper>
       <Post title='Linux 基础命令1•基本文件和目录操作' date='Nov 30, 2021' />
+      <Post title='Linux 基础命令1•基本文件和目录操作' date='Nov 30, 2021' />
+      <Post title='Linux 基础命令1•基本文件和目录操作' date='Nov 30, 2021' />
+      <Post title='Linux 基础命令1•基本文件和目录操作' date='Nov 30, 2021' />
+      <Post title='Linux 基础命令1•基本文件和目录操作' date='Nov 30, 2021' />
     </Wrapper>
   );
 };
