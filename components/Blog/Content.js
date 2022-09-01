0f8ead57330e1144942c2a2219a4f3886ab3b516
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-01 10:53:40 +0800

    feat: add psot title

--- components/Blog/Content.js
+++ components/Blog/Content.js
@@ -1,9 +1,11 @@
 import Wrapper from '../Wrapper';
 import Post from './Post';
+import styles from '../../styles/Blog.module.css';
 
 const content = ({ posts }) => {
   return (
     <Wrapper>
+      <h2 className={styles.title}>Posts</h2>
       {posts.map((post) => (
         <Post title={post.title} date={post.date} id={post.id} key={post.id} />
       ))}
