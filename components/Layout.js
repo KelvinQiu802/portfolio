c5bcb18a2de0c776865bd07cbbd6ece8d66994ba
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-03 23:31:35 +0800

    feat: favicon

--- components/Layout.js
+++ components/Layout.js
@@ -14,6 +14,7 @@ const Layout = ({ children }) => {
   return (
     <div className='layout' color-mode={mode}>
       <Head>
+        <link rel='shortcut icon' href='/favicon.svg' type='image/x-icon' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
         <link
