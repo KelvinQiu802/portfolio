a15c5f8841873cf8637d523997782e9c0621d240
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-03 22:34:40 +0800

    feat: Project page

--- pages/project.js
+++ pages/project.js
@@ -9,32 +9,55 @@ const Project = () => {
         <p className={styles.title}>Projects</p>
         <div className={styles.list}>
           <Item
-            name='Battleship'
+            name='battleship'
             description='A Battleship Game on Web with React.'
             icon='&#xe928;'
+            repo='https://github.com/KelvinQiu802/battleship'
           />
           <Item
-            name='Battleship'
-            description='A Battleship Game on Web with React.'
-            icon='&#xe928;'
+            name='kanban-react'
+            description='A Kanban Web App Built with React.'
+            icon='&#xe9cb;'
+            repo='https://github.com/KelvinQiu802/kanban-react'
           />
           <Item
-            name='Battleship'
-            description='A Battleship Game on Web with React.'
-            icon='&#xe928;'
+            name='micro-react'
+            description='A micro React that implements React core concepts.'
+            icon='&#xe64b;'
+            repo='https://github.com/KelvinQiu802/micro-react'
+          />
+          <Item
+            name='mini-redux'
+            description='用最简单的JavaScript实现Redux核心功能'
+            icon='&#xe66c;'
+            repo='https://github.com/KelvinQiu802/mini-redux'
+          />
+          <Item
+            name='bookstore'
+            description='A Book Searching Website using Google Book API.'
+            icon='&#xe61f;'
+            repo='https://github.com/KelvinQiu802/bookstore'
+          />
+          <Item
+            name='your-cat'
+            description='A website that will help you to choose the best breed of cat for you by asking you a few questions.'
+            icon='&#xec8d;'
+            repo='https://github.com/KelvinQiu802/your-cat'
           />
         </div>
         <p className={styles.title}>Demo</p>
         <div className={styles.list}>
           <Item
-            name='Battleship'
-            description='A Battleship Game on Web with React.'
-            icon='&#xe928;'
+            name='163-music-downloader'
+            description='A tampermonkey script to download 163 music.'
+            icon='&#xe6b6;'
+            repo='https://github.com/KelvinQiu802/163MusicDownloaderScript'
           />
           <Item
-            name='Battleship'
-            description='A Battleship Game on Web with React.'
-            icon='&#xe928;'
+            name='dark-mode-toggle'
+            description='A dark mode demo.'
+            icon='&#xe65a;'
+            repo='https://github.com/KelvinQiu802/dark-mode-toggle'
           />
         </div>
       </div>
