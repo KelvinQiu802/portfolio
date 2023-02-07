f7ff0b15625e1215b724b8ad0180a0a8db8ed78e
KelvinQiu <kelvinqiu802@outlook.com>
2023-02-07 23:47:54 +0800

    feat: generate every year

--- components/Blog/Content.js
+++ components/Blog/Content.js
@@ -2,20 +2,38 @@ import Wrapper from '../Wrapper';
 import Post from './Post';
 import styles from '../../styles/Blog.module.css';
 
+const FIRST_YEAR = 2022;
+function getYears() {
+  const date = new Date();
+  let currentYear = date.getFullYear();
+  const allYears = [];
+  while (currentYear >= FIRST_YEAR) {
+    allYears.push(currentYear);
+    currentYear--;
+  }
+  return allYears;
+}
+
 const content = ({ posts }) => {
   return (
     <Wrapper>
-      <p className={styles.year}>2022</p>
-      {posts
-        .filter((post) => post.date.includes('2022'))
-        .map((post) => (
-          <Post
-            title={post.title}
-            date={post.date}
-            id={post.id}
-            key={post.id}
-          />
-        ))}
+      {getYears().map((year) => (
+        <>
+          <p className={styles.year} key={year}>
+            {year}
+          </p>
+          {posts
+            .filter((post) => post.date.includes(year))
+            .map((post) => (
+              <Post
+                title={post.title}
+                date={post.date}
+                id={post.id}
+                key={post.id}
+              />
+            ))}
+        </>
+      ))}
     </Wrapper>
   );
 };
