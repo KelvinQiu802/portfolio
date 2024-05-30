9813802b38525e52c5316a85f81280c9bae9677b
KelvinQiu <kelvinqiu802@outlook.com>
2024-05-30 20:44:11 +0800

    feat: rm year

--- components/Blog/Content.js
+++ components/Blog/Content.js
@@ -1,6 +1,5 @@
 import Wrapper from '../Wrapper';
 import Post from './Post';
-import styles from '../../styles/Blog.module.css';
 
 const FIRST_YEAR = 2022;
 function getYears() {
@@ -17,22 +16,8 @@ function getYears() {
 const content = ({ posts }) => {
   return (
     <Wrapper>
-      {getYears().map((year) => (
-        <>
-          <p className={styles.year} key={year}>
-            {year}
-          </p>
-          {posts
-            .filter((post) => post.date.includes(year))
-            .map((post) => (
-              <Post
-                title={post.title}
-                date={post.date}
-                id={post.id}
-                key={post.id}
-              />
-            ))}
-        </>
+      {posts.map((post) => (
+        <Post title={post.title} date={post.date} id={post.id} key={post.id} />
       ))}
     </Wrapper>
   );
